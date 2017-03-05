548b2cd30aeee67a3c6bc28a7d7c320f18bcdf01
Hxmic <mic7512@163.com>
2017-03-05 18:36:18 +0800

    add-tab-or-ve

--- Complete/script/style.js
+++ Complete/script/style.js
@@ -10,15 +10,27 @@ function hideSubMenu(li) {
     submenu.style.display = "none";
 }
 
-function showDiv(n,m) {
-  var liLg=document.getElementsByClassName("li");
-  for(var i=1;i<=liLg.length;i++){
-    var allDiv = document.getElementById("div"+i);
-    var allLi = document.getElementById("li"+i);
-    allDiv.style.display = "none";
-    allLi.style.background="white";
-  }
-  m.style.background="red";
-  var obj = document.getElementById("div"+n);
-  obj.style.display = "block";
+function showDiv(n, m) {
+    var liLg = document.getElementsByClassName("li");
+    for (var i = 1; i <= liLg.length; i++) {
+        var allDiv = document.getElementById("div" + i);
+        var allLi = document.getElementById("li" + i);
+        allDiv.style.display = "none";
+        allLi.style.background = "darkgray";
+    }
+    m.style.background = "white";
+    var obj = document.getElementById("div" + n);
+    obj.style.display = "block";
+}
+function showDiv1(n, m) {
+    var liLg = document.getElementsByClassName("li1");
+    for (var i = 1; i <= liLg.length; i++) {
+        var allDiv = document.getElementById("Div" + i);
+        var allLi = document.getElementById("Li" + i);
+        allDiv.style.display = "none";
+        allLi.style.color = "darkgray";
+    }
+    m.style.color = "green";
+    var obj = document.getElementById("Div" + n);
+    obj.style.display = "block";
 }
